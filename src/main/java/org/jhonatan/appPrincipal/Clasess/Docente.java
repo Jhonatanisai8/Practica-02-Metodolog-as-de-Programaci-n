91ebe7106aad675f946ce59153f91054e6e25290
jho-my <ojedasnachezjhonatan@gmail.com>
2024-06-01 11:56:39 -0500

    clase docente: esta es clase hija de la clase persona. establecemos sus atributos y constructor

--- src/main/java/org/jhonatan/appPrincipal/Clasess/Docente.java
+++ src/main/java/org/jhonatan/appPrincipal/Clasess/Docente.java
@@ -4,6 +4,18 @@ package org.jhonatan.appPrincipal.Clasess;
  *
  * @author Jhonatan
  */
-public class Docente {
-    
+public class Docente extends Persona {
+
+    private int grado;
+    private int horas;
+    private double costoHora;
+
+    public Docente(int idPersona, String nombre, String apellidos, String categoria,
+            int grado, int horas, double costoHora) {
+        super(idPersona, nombre, apellidos, categoria);
+        this.grado = grado;
+        this.horas = horas;
+        this.costoHora = costoHora;
+    }
+
 }
