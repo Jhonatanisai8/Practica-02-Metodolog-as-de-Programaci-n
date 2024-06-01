f6cb26e1d4d95ccdffa3d7e11f3e85abe82ee4af
jho-my <ojedasnachezjhonatan@gmail.com>
2024-06-01 12:04:10 -0500

    Clase estudiante: atributos,consctructor y getters,setters

--- src/main/java/org/jhonatan/appPrincipal/Clasess/Estudiante.java
+++ src/main/java/org/jhonatan/appPrincipal/Clasess/Estudiante.java
@@ -4,6 +4,23 @@ package org.jhonatan.appPrincipal.Clasess;
  *
  * @author Jhonatan
  */
-public class Estudiante {
-    
+public class Estudiante extends Persona {
+
+    //atributos
+    private String escuela;
+
+    //constructor
+    public Estudiante(int idPersona, String nombre, String apellidos, String categoria, String escuela) {
+        super(idPersona, nombre, apellidos, categoria);
+        this.escuela = escuela;
+    }
+
+    public String getEscuela() {
+        return escuela;
+    }
+
+    public void setEscuela(String escuela) {
+        this.escuela = escuela;
+    }
+
 }
