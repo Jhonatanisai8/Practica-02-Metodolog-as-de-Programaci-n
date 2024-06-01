4d96a1d311b5a84e8cd3cf1db8d51a5b081cc0dd
jho-my <ojedasnachezjhonatan@gmail.com>
2024-06-01 12:00:16 -0500

    clase docente: meetodo para calcular su sueldo en base a su categoria y el costo por hora de esa categoria

--- src/main/java/org/jhonatan/appPrincipal/Clasess/Docente.java
+++ src/main/java/org/jhonatan/appPrincipal/Clasess/Docente.java
@@ -18,4 +18,38 @@ public class Docente extends Persona {
         this.costoHora = costoHora;
     }
 
+    public int getGrado() {
+        return grado;
+    }
+
+    public void setGrado(int grado) {
+        this.grado = grado;
+    }
+
+    public int getHoras() {
+        return horas;
+    }
+
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+
+    public double getCostoHora() {
+        return costoHora;
+    }
+
+    public void setCostoHora(double costoHora) {
+        this.costoHora = costoHora;
+    }
+
+    //metodos
+    public double calcularSueldo() {
+        double sueldo = 0;
+        switch (this.getCategoria()) {
+            case "A" -> sueldo = (horas * 80);
+            case "B" -> sueldo = (horas * 70);
+            case "C" -> sueldo = (horas * 50);
+        }
+        return sueldo;
+    }
 }
